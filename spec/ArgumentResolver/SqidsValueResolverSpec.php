960ca598739dc6b8559f848befb2fd6694c8d9f6
Mathias STRASSER <contact@roukmoute.fr>
2023-07-31 19:47:31 +0200

    Init SqidsValueResolver

---
+++ spec/ArgumentResolver/SqidsValueResolverSpec.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace spec\Roukmoute\SqidsBundle\ArgumentResolver;
+
+use PhpSpec\ObjectBehavior;
+use Roukmoute\SqidsBundle\ArgumentResolver\SqidsValueResolver;
+use Sqids\Sqids;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+class SqidsValueResolverSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(new Sqids());
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(SqidsValueResolver::class);
+    }
+
+    function it_resolves_sqids_argument()
+    {
+        $request = new Request([], [], ['foo' => 'U9']);
+        $argumentMetadata = new ArgumentMetadata('foo', Sqids::class, false, false, null);
+
+        $this->resolve($request, $argumentMetadata)->shouldReturn([1]);
+    }
+}
---
+++ src/ArgumentResolver/SqidsValueResolver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Roukmoute\SqidsBundle\ArgumentResolver;
+
+use Sqids\Sqids;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SqidsValueResolver implements ValueResolverInterface
+{
+    public function __construct(private Sqids $sqids)
+    {
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    {
+        $value = $request->attributes->get($argument->getName());
+
+        return $this->sqids->decode($value);
+    }
+}
