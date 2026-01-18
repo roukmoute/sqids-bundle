02b8198e5337b3156001fbf063a648bb74dca16e
Mathias STRASSER <contact@roukmoute.fr>
2026-01-18 11:59:03 +0100

    Fix static analysis and lowest deps issues
    - Add declare(strict_types=1) to RoukmouteSqidsExtension.php
    - Fix code style in RoukmouteSqidsExtension.php
    - Add phpspec/prophecy ^1.17 constraint to fix '\static' bug in lowest deps

--- src/DependencyInjection/RoukmouteSqidsExtension.php
+++ src/DependencyInjection/RoukmouteSqidsExtension.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Roukmoute\SqidsBundle\DependencyInjection;
 
 use Sqids\Sqids;
@@ -12,13 +14,14 @@ class RoukmouteSqidsExtension extends ConfigurableExtension
 {
     protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
-        $loader = new Loader\PhpFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
+        $loader = new Loader\PhpFileLoader($container, new FileLocator(__DIR__ . '/../../Resources/config'));
         $loader->load('services.php');
 
         $container->getDefinition(Sqids::class)
             ->setPublic(true)
             ->setArgument(0, $mergedConfig['alphabet'])
             ->setArgument(1, $mergedConfig['min_length'])
-            ->setArgument(2, $mergedConfig['blocklist']);
+            ->setArgument(2, $mergedConfig['blocklist'])
+        ;
     }
 }
