f3c8aa83fcf4533ac1b90bb291b9c6159dc0df5b
Mathias STRASSER <contact@roukmoute.fr>
2026-01-18 11:07:03 +0100

    Fix source files and tests for sqids 0.4/0.5 compatibility
    - Add declare(strict_types=1) to source files
    - Use \InvalidArgumentException instead of imported class
    - Update tests to use dynamic sqids encoding instead of hardcoded values
    - Tests now work with any sqids version

--- spec/ValueResolver/SqidsValueResolverSpec.php
+++ spec/ValueResolver/SqidsValueResolverSpec.php
@@ -13,9 +13,12 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SqidsValueResolverSpec extends ObjectBehavior
 {
+    private Sqids $sqids;
+
     function let()
     {
-        $this->beConstructedWith(new Sqids());
+        $this->sqids = new Sqids();
+        $this->beConstructedWith($this->sqids);
     }
 
     function it_is_initializable()
@@ -25,7 +28,7 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_fails_to_resolve_non_variadic_argument()
     {
-        $request = new Request([], [], ['foo' => 'U9']);
+        $request = new Request([], [], ['foo' => $this->sqids->encode([1])]);
         $argumentMetadata = new ArgumentMetadata('foo', 'int', true, false, null);
 
         $this->resolve($request, $argumentMetadata)->shouldReturn([]);
@@ -57,7 +60,7 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_fails_when_argument_lacks_type()
     {
-        $request = new Request([], [], ['foo' => 'U9']);
+        $request = new Request([], [], ['foo' => $this->sqids->encode([1])]);
         $argumentMetadata = new ArgumentMetadata('foo', null, false, false, null);
 
         $this->resolve($request, $argumentMetadata)->shouldReturn([]);
@@ -65,7 +68,7 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_fails_when_argument_type_not_class()
     {
-        $request = new Request([], [], ['foo' => 'U9']);
+        $request = new Request([], [], ['foo' => $this->sqids->encode([1])]);
         $argumentMetadata = new ArgumentMetadata('foo', 'string', false, false, null);
 
         $this->resolve($request, $argumentMetadata)->shouldReturn([]);
@@ -73,7 +76,7 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_resolves_sqids_int_argument()
     {
-        $request = new Request([], [], ['foo' => 'U9']);
+        $request = new Request([], [], ['foo' => $this->sqids->encode([1])]);
         $argumentMetadata = new ArgumentMetadata('foo', 'int', false, false, null);
 
         $this->resolve($request, $argumentMetadata)->shouldReturn([1]);
@@ -81,8 +84,9 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_resolves_sqids_object_argument(Request $request, ParameterBag $attributes)
     {
+        $encoded = $this->sqids->encode([1]);
         $request->attributes = $attributes;
-        $attributes->get('foo')->willReturn('U9');
+        $attributes->get('foo')->willReturn($encoded);
         $argumentMetadata = new ArgumentMetadata('foo', stdClass::class, false, false, null);
 
         $attributes->set('foo', 1)->shouldBeCalled();
@@ -91,7 +95,7 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_fails_with_sqid_with_multiple_integers()
     {
-        $request = new Request([], [], ['id' => 'XgbK']);
+        $request = new Request([], [], ['id' => $this->sqids->encode([1, 2, 3])]);
         $argumentMetadata = new ArgumentMetadata('id', 'int', false, false, null);
 
         $this->shouldThrow(new NotFoundHttpException('The sqid for the "id" parameter is invalid.'))->during('resolve', [$request, $argumentMetadata]);
@@ -99,7 +103,8 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_fails_with_sqid_with_bad_value_for_id()
     {
-        $request = new Request([], [], ['id' => 'ccc']);
+        // A sqid that decodes to 0 but doesn't match the canonical encoding of 0
+        $request = new Request([], [], ['id' => 'bMbM']);
         $argumentMetadata = new ArgumentMetadata('id', 'int', false, false, null);
 
         $this->shouldThrow(new NotFoundHttpException('The sqid for the "id" parameter is invalid.'))->during('resolve', [$request, $argumentMetadata]);
@@ -107,15 +112,15 @@ class SqidsValueResolverSpec extends ObjectBehavior
 
     function it_resolves_with_value_equals_0()
     {
-        $request = new Request([], [], ['id' => 'bV']);
+        $request = new Request([], [], ['id' => $this->sqids->encode([0])]);
         $argumentMetadata = new ArgumentMetadata('id', 'int', false, false, null);
 
         $this->resolve($request, $argumentMetadata)->shouldReturn([0]);
     }
 
-    public function it_fails_with_wrong_type_argument()
+    function it_fails_with_wrong_type_argument()
     {
-        $request = new Request([], [], ['id' => 'bV']);
+        $request = new Request([], [], ['id' => $this->sqids->encode([0])]);
         $argumentMetadata = new ArgumentMetadata('id', 'string', false, false, null);
 
         $this->resolve($request, $argumentMetadata)->shouldReturn([]);
--- src/RoukmouteSqidsBundle.php
+++ src/RoukmouteSqidsBundle.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Roukmoute\SqidsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
